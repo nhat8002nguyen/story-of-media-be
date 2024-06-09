dcd7b7959efee42150034f00823a0e971148a4a8
nhat8002nguyen <nv.nhat8002@gmail.com>
2024-06-09 10:26:48 +0700

    update db connect str

--- user-service/src/models/database.go
+++ user-service/src/models/database.go
@@ -12,9 +12,14 @@ var Db *sql.DB
 
 func InitDB() {
 	var err error
-	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"))
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"))
 	Db, err = sql.Open("postgres", connStr)
+	fmt.Println("Connection String:", connStr)
 	if err != nil {
 		panic(err)
 	}
