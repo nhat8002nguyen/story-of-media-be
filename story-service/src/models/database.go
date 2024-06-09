43a0a7fbbd509ae5c40ba6235802b17330356bc3
nhat8002nguyen <nv.nhat8002@gmail.com>
2024-06-09 11:05:20 +0700

    change dbstring story-service

--- story-service/src/models/database.go
+++ story-service/src/models/database.go
@@ -12,8 +12,9 @@ var Db *sql.DB
 
 func InitDB() {
 	var err error
-	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"))
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"))
 	Db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
